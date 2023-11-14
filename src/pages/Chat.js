e99e4be217de6e5096b01cfcdb5e45b41466e4d8
leonardobuckalves <leonardobuckalves@gmail.com>
2023-11-14 15:56:39 -0300

    opções iniciais chat

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,111 +1,77 @@
-import React, { useEffect, useState } from "react";
-import {
-  View,
-  FlatList,
-  StyleSheet,
-  TextInput,
-  Button,
-  Text,
-} from "react-native";
-import { List, Divider } from "react-native-paper";
-import { useNavigation } from "@react-navigation/native";
-import { useUser } from "../context/UserContext.js";
-import { useIsFocused } from "@react-navigation/native";
-import { getMessages, postMessages } from "../services/chat.services";
-import HeaderPages from "../components/headerPages";
-import MyButton from "../components/button.js";
+import React, { useState, useCallback, useEffect } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = () => {
-  const navigation = useNavigation();
-
-  const { name, userId } = useUser();
-
-  const isFocused = useIsFocused();
-
-  const [mensagem, setMensagem] = useState([]);
-
-  const [text, setText] = useState("");
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    updateMessages();
-  }, [isFocused]);
+    setMessages([
+      {
+        _id: Math.random(),
+        text: 'Bem vindo ao Smart Wallet helpbot, como posso ajudar?',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'Smart Wallet',
+        },
+      },
+    ]);
+  }, []);
 
-  const updateMessages = () => {
-    getMessages().then((dados) => {
-      const mensagens = dados.filter((user) => user.userId === userId);
-      setMensagem(mensagens);
-    });
-  };
+  const onSend = useCallback((newMessages = []) => {
+    const uniqueMessages = newMessages.map((message) => ({
+      ...message,
+      _id: Math.random(), // Generate a unique ID for each message
+    }));
 
-  const handleSendMessage = () => {
-    postMessages({
-      nome: name,
-      mensagem: text,
-      userId: userId,
-    }).then((res) => {
-      updateMessages();
-      setText("");
-      navigation.navigate("Chat");
-    });
-  };
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, uniqueMessages)
+    );
+
+    if (newMessages[0].text === 'Lançamentos') {
+      const responseMessages = [
+        {
+          _id: Math.random(),
+          text:
+            'Para realizar um novo Lançamento, basta apertar o botão "Lançamentos" no rodapé da tela inicial!',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Smart Wallet',
+          },
+        },
+      ];
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, responseMessages)
+      );
+    } else {
+      const responseMessages = [
+        {
+          _id: Math.random(),
+          text:
+            'Desculpe, não entendi o que você está tentando me dizer. Tente digitar "Lançamentos" ou "Extrato"',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Smart Wallet',
+          },
+        },
+      ];
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, responseMessages)
+      );
+    }
+  }, []);
 
   return (
-    <>
-      <HeaderPages navigation={navigation} />
-      <View style={styles.container}>
-        <View style={styles.body}>
-          <FlatList
-            data={mensagem}
-            renderItem={({ item }) => (
-              <List.Item title={name + ":"} description={item.mensagem} />
-            )}
-            keyExtractor={(item) => item.id}
-          />
-        </View>
-        <View style={{ margin: 10, marginBottom: 10, marginTop: 10 }}>
-          <Divider
-            style={{
-              borderColor: "blue",
-              borderWidth: 0.5,
-              marginTop: 5,
-              marginBottom: 5,
-              marginLeft: 5,
-              marginRight: 15,
-            }}
-          />
-          <TextInput
-          placeholder="digite aqui sua mensagem.."
-            mode="outlined"
-            style={{
-              marginLeft: 10,
-              marginRight: 10,
-              marginBottom: 5,
-              fontSize: 14,
-              height: 40,
-              backgroundColor: "Whitesmoke",
-            }}
-          />
-
-<MyButton onPress={handleSendMessage} title="Enviar" color="darkblue"/>
-        </View>
-      </View>
-    </>
+    <GiftedChat
+      messages={messages}
+      onSend={(newMessages) => onSend(newMessages)}
+      user={{
+        _id: 1,
+      }}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFF",
-  },
-  body: {
-    flex: 1,
-    backgroundColor: "#FFF",
-    margin: 8,
-  },
-  enviar: {
-    paddingLeft: 10,
-},
-});
 
 export default Chat;
