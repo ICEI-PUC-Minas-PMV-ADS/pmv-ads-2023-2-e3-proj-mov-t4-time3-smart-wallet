2fedbebbfa7c01e6f9878f6fb1955fe12c48bc70
leonardobuckalves <leonardobuckalves@gmail.com>
2023-11-15 14:49:58 -0300

    Finalização chat
    refatoração do helpchat
    remoção do chat no arquivo db.json

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
 import { useNavigation } from '@react-navigation/native';
 import HeaderPages from '../components/headerPages';
+import { dicasEconomia } from '../assets/textos/comoEconomizar';
 
 const Chat = () => {
 
@@ -13,7 +14,7 @@ const Chat = () => {
     setMessages([
       {
         _id: Math.random(),
-        text: 'Bem vindo ao Smart Wallet helpbot, como posso ajudar?',
+        text: 'Bem vindo ao Smart Wallet helpchat, como posso ajudar?',
         createdAt: new Date(),
         user: {
           _id: 2,
@@ -23,47 +24,90 @@ const Chat = () => {
     ]);
   }, []);
 
-  const onSend = useCallback((newMessages = []) => {
-    const uniqueMessages = newMessages.map((message) => ({
-      ...message,
-      _id: Math.random(), // Generate a unique ID for each message
-    }));
-
+  const appendMessages = (responseMessages) => {
     setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, uniqueMessages)
+      GiftedChat.append(previousMessages, responseMessages)
     );
+  };
 
-    if (newMessages[0].text === 'Lançamentos') {
-      const responseMessages = [
-        {
-          _id: Math.random(),
-          text:
-            'Para realizar um novo Lançamento, basta apertar o botão "Lançamentos" no rodapé da tela inicial!',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'Smart Wallet',
-          },
+  const createResponseMessage = (texto) => {
+    return [
+      {
+        _id: Math.random(),
+        text: texto,
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'Smart Wallet',
         },
-      ];
-      setMessages((previousMessages) =>
-        GiftedChat.append(previousMessages, responseMessages)
+      },
+    ];
+  };
+
+  const onSend = useCallback((newMessages = []) => {
+    const formatedMessage = newMessages[0].text.toUpperCase();
+    appendMessages(newMessages)
+    if (
+      formatedMessage.includes('INSERIR LANÇAMENTO') ||
+      formatedMessage.includes('INSERIR LANÇAMENTOS') ||
+      formatedMessage.includes('INSERIR')
+    ) {
+      appendMessages(
+        createResponseMessage('Vá até a tela inicial para inserir um novo Lançamento,' +
+          'localize o rodapé e selecione o botão "Lançamento". ' +
+          'Insira os dados e aperte salvar para concluir a operação.')
+      );
+
+    } else if (
+      formatedMessage.includes('EDITAR LANÇAMENTO') ||
+      formatedMessage.includes('EDITAR LANÇAMENTOS') ||
+      formatedMessage.includes('EDITAR')
+    ) {
+      appendMessages(
+        createResponseMessage('Para editar um lançamento vá até a tela inicial, ' +
+        'localize o rodapé e selecione o botão "Extrato". ' +
+        'Nessa tela, localize o lançamento que deseja alterar e selecione o icone do lápis, ' +
+        'realize as alterações necessárias e clique em salvar para modificar a operação.')
+      );
+
+    } else if (
+      formatedMessage.includes('EXCLUIR LANÇAMENTO') ||
+      formatedMessage.includes('EXCLUIR LANÇAMENTOS') || 
+      formatedMessage.includes('EXCLUIR')
+    ) {
+      appendMessages(
+        createResponseMessage('Para excluir um lançamento vá até a tela inicial, ' +
+        'localize o rodapé e selecione o botão "Extrato". ' +
+        'Nessa tela, localize o lançamento que deseja excluir e ' +
+        'selecione o icone da lixeira e confirme a operação para remover o lançamento.')
+      );
+
+    } else if (
+      formatedMessage.includes('EXTRATO')
+    ) {
+      appendMessages(createResponseMessage('Vá até a tela inicial, localize o rodapé e ' +
+      'selecione o botão de "Extrato" para acessar o extrato de sua conta.'));
+
+    } else if (
+      formatedMessage.includes('COMO ECONOMIZAR') ||
+      formatedMessage.includes('ECONOMIA') ||
+      formatedMessage.includes('DICA') ||
+      formatedMessage.includes('DICAS') ||
+      formatedMessage.includes('ECONOMIZAR')) {
+      const dicaAleatoria = Math.floor(Math.random() * 13);
+      appendMessages(
+        createResponseMessage(dicasEconomia[dicaAleatoria])
       );
     } else {
-      const responseMessages = [
-        {
-          _id: Math.random(),
-          text:
-            'Desculpe, não entendi o que você está tentando me dizer. Tente digitar "Lançamentos" ou "Extrato"',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'Smart Wallet',
-          },
-        },
-      ];
-      setMessages((previousMessages) =>
-        GiftedChat.append(previousMessages, responseMessages)
+      appendMessages(
+        createResponseMessage(
+          'Desculpe, não entendi o que você está tentando me dizer. Tente perguntar coisas como:\n\n' +
+            '"Como inserir um lançamento"\n' +
+            '"Como editar um lançamento"\n' +
+            '"Como excluir um lançamento"\n' +
+            '"Como acessar meu extrato"\n' +
+            '"Como economizar" ou "Dicas"'
+        )
       );
     }
   }, []);
