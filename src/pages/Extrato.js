41775e5c06b9a57677f85965ee36f0d6f78a60d4
Leonardo Lima <leodmelo@hotmail.com>
2023-11-17 16:44:37 -0300

    update

--- src/pages/Extrato.js
+++ src/pages/Extrato.js
@@ -1,42 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { Card, Divider } from 'react-native-paper';
-import { View, Text, StyleSheet, TouchableOpacity, Button, ScrollView, TextInput, Modal, Image } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import FooterNavigation from '../components/footer';
-import HeaderPages from '../components/headerPages';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import { useIsFocused } from '@react-navigation/native';
-import { useUser } from '../context/UserContext.js';
-import { getLancamentos, deleteLancamento } from '../services/lancamento.services';
+import React, { useState, useEffect } from "react";
+import { Card, Divider } from "react-native-paper";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Button,
+  ScrollView,
+  TextInput,
+  Modal,
+  Image,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import FooterNavigation from "../components/footer";
+import HeaderPages from "../components/headerPages";
+import { useIsFocused } from "@react-navigation/native";
+import { useUser } from "../context/UserContext.js";
+import {
+  getLancamentos,
+  deleteLancamento,
+} from "../services/lancamento.services";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Extrato = () => {
-
   const navigation = useNavigation();
   const isFocused = useIsFocused();
   const { userId } = useUser();
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const [lancamentos, setLancamentos] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
   const [itemToDelete, setItemToDelete] = useState(null);
 
-  // useEffect(() => {
-  //   const jsonFileUrl = 'https://smartwallet.loca.lt/extrato';
-
-  //   fetch(jsonFileUrl)
-  //     .then((response) => response.text())
-  //     .then((text) => {
-  //       console.log('Response:', text); // Exibe a resposta no console para depuração
-  //       return JSON.parse(text);
-  //     })
-  //     .then((jsonData) => setData(jsonData))
-  //     .catch((error) => console.error('Error reading JSON:', error));
-
-  // }, []);
-
   useEffect(() => {
-
-    getLancamentos().then(dados => {
-      const lancamentos = dados.filter(user => user.userId === userId);
+    getLancamentos().then((dados) => {
+      const lancamentos = dados.filter((user) => user.userId === userId);
       setLancamentos(lancamentos);
     });
   }, [isFocused]);
@@ -58,136 +55,161 @@ const Extrato = () => {
   const handleConfirmDelete = () => {
     closeModal();
     if (itemToDelete) {
-      deleteLancamento(itemToDelete.id).then(res => {
-        const updatedLancamentos = lancamentos.filter((l) => l.id !== itemToDelete.id);
+      deleteLancamento(itemToDelete.id).then((res) => {
+        const updatedLancamentos = lancamentos.filter(
+          (l) => l.id !== itemToDelete.id
+        );
         setLancamentos(updatedLancamentos);
-        navigation.navigate('Extrato');
+        navigation.navigate("Extrato");
       });
     }
   };
 
   return (
-
     <View style={styles.container}>
       <HeaderPages navigation={navigation} />
-      <ScrollView >
-      <View>
-
+      <ScrollView>
         <View>
-          <Text style={styles.titlePage}>
-            Extrato
-          </Text>
-        </View>
-
-        <View style={styles.searchContainer}>
-          <TextInput
-            style={styles.searchInput}
-            placeholder="Pesquisar..."
-            value={searchText}
-            onChangeText={(text) => setSearchText(text)}
-          />
-        </View>
-        <Divider
-          style={{
-            borderColor: "darkblue",
-            borderWidth: 0.5,
-          }}
-        />
-
-        <ScrollView>
-          {lancamentos
-    .filter((item) => {
-      const includesSearchText = item.classificacao.toLowerCase().includes(searchText.toLowerCase()) ||
-                                (item.valor || "").toString().includes(searchText) ||
-                                item.recorrente.toLowerCase().includes(searchText.toLowerCase()) ||
-                                item.status.toLowerCase().includes(searchText.toLowerCase()) ||
-                                (item.descricao || "").toLowerCase().includes(searchText.toLowerCase()) ||
-                                (item.tipo.toLowerCase() === 'receita' && 'receita'.includes(searchText.toLowerCase())) ||
-                                (item.tipo.toLowerCase() === 'despesa' && 'despesa'.includes(searchText.toLowerCase()));
-
-      return includesSearchText;
-    })
-            .map((item) => (
-              <Card key={item.id} style={styles.card}>
-                <Text style={{ fontWeight: "bold", color: "darkblue" }}>
-                  Vencimento: {item.dataVencimento || "N/A"}
-                </Text>
-                <Divider
-                  style={{
-                    borderColor: "gray",
-                    borderWidth: 0.5,
-                    marginTop: 5,
-                    marginBottom: 5,
-                    marginRight: 15,
-                  }}
-                />
-                <Text
-                  style={{
-                    color: item.tipo === "Receita" ? "green" : "red",
-                    fontWeight: "bold",
-                  }}
-                >
-                  • Tipo: {item.tipo}
-                </Text>
-                <Text>• Classificação: {item.classificacao}</Text>
-                <Text>• Valor: {item.valor || "N/A"}</Text>
-                <Text>• Recorrente: {item.recorrente}</Text>
-                <Text
-                  style={{
-                    color: item.status === "Efetivado" ? "green" : "red",
-                    fontWeight: "bold",
-                  }}
-                >
-                  • Status: {item.status}
-                </Text>
-                <Text>• Descrição: {item.descricao || "N/A"}</Text>
-                <View style={styles.buttonContainer}>
-                  <TouchableOpacity
-                    style={styles.editButton}
-                    onPress={() =>
-                      navigation.navigate("Lancamento", {item})
-                    }
-                  >
-                    <Icon name="edit" size={20} color="darkblue" />
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    style={styles.deleteButton}
-                    onPress={() => handleDelete(item)}
-                  >
-                    <Icon name="delete" size={20} color="darkblue" />
-                  </TouchableOpacity>
-                </View>
-              </Card>
-            ))}
-        </ScrollView>
-      </View>
-
-      <Modal
-        visible={modalVisible}
-        animationType="slide"
-        transparent={true}
-      >
-        <View style={styles.modalContainer}>
-        <Image
-            source={require('../assets/SmartWallet.png')}
-            style={{
-              width: 250,
-              height: 150,
-              marginBottom: 30,
-              alignSelf: 'center',
-            }}
-            resizeMode="contain"
-          />
-          <Text style={styles.modalText}>Tem certeza que deseja excluir?</Text>
-          <View style={styles.modalButtonContainer}>      
-            <Button title="Cancelar" onPress={closeModal} />
-            <Button title="Confirmar" onPress={handleConfirmDelete} />
+          <View>
+            <Text style={styles.titlePage}>Extrato</Text>
           </View>
+
+          <View style={styles.searchContainer}>
+            <TextInput
+              style={styles.searchInput}
+              placeholder="Pesquisar..."
+              value={searchText}
+              onChangeText={(text) => setSearchText(text)}
+            />
+          </View>
+          <Divider
+            style={{
+              borderColor: "darkblue",
+              borderWidth: 0.5,
+            }}
+          />
+
+          <ScrollView>
+            {lancamentos
+              .filter((item) => {
+                const includesSearchText =
+                  item.classificacao
+                    .toLowerCase()
+                    .includes(searchText.toLowerCase()) ||
+                  (item.valor || "").toString().includes(searchText) ||
+                  item.recorrente
+                    .toLowerCase()
+                    .includes(searchText.toLowerCase()) ||
+                  item.status
+                    .toLowerCase()
+                    .includes(searchText.toLowerCase()) ||
+                  (item.descricao || "")
+                    .toLowerCase()
+                    .includes(searchText.toLowerCase()) ||
+                  (item.tipo.toLowerCase() === "receita" &&
+                    "receita".includes(searchText.toLowerCase())) ||
+                  (item.tipo.toLowerCase() === "despesa" &&
+                    "despesa".includes(searchText.toLowerCase()));
+
+                return includesSearchText;
+              })
+              .map((item) => (
+                <Card key={item.id} style={styles.card}>
+                  <Text style={{ fontWeight: "bold", color: "darkblue" }}>
+                    Vencimento: {item.dataVencimento || "N/A"}
+                  </Text>
+                  <Divider
+                    style={{
+                      borderColor: "gray",
+                      borderWidth: 0.5,
+                      marginTop: 5,
+                      marginBottom: 5,
+                      marginRight: 15,
+                    }}
+                  />
+                  <Text
+                    style={{
+                      fontWeight: "bold",
+                      color: item.tipo === "Receita" ? "green" : "red",
+                    }}
+                  >
+                    {item.tipo === "Receita" ? (
+                      <>
+                        <Icon name="upload" size={20} color="green" /> Tipo:{" "}
+                        {item.tipo}
+                      </>
+                    ) : (
+                      <>
+                        <Icon name="download" size={20} color="red" /> Tipo:{" "}
+                        {item.tipo}
+                      </>
+                    )}
+                  </Text>
+
+                  <Text>• Classificação: {item.classificacao}</Text>
+                  <Text>• Valor: {item.valor || "N/A"}</Text>
+                  <Text>• Recorrente: {item.recorrente}</Text>
+                  <Text
+                    style={{
+                      color: item.status === "Efetivado" ? "green" : "red",
+                      fontWeight: "bold",
+                      backgroundColor:
+                        item.status === "Efetivado" ? "lightgreen" : "pink",
+                      padding: 5,
+                      borderRadius: 5,
+                      flexDirection: "row",
+                      alignItems: "center",
+                    }}
+                  >
+                    • Status: {item.status}
+                  </Text>
+
+                  <Text>• Descrição: {item.descricao || "N/A"}</Text>
+                  <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                      style={styles.editButton}
+                      onPress={() =>
+                        navigation.navigate("Lancamento", { item })
+                      }
+                    >
+                      <Icon name="edit" size={20} color="darkblue" />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={styles.deleteButton}
+                      onPress={() => handleDelete(item)}
+                    >
+                      <Icon name="trash" size={20} color="darkblue" />
+                    </TouchableOpacity>
+                  </View>
+                </Card>
+              ))}
+          </ScrollView>
         </View>
-      </Modal>
+
+        <Modal visible={modalVisible} animationType="slide" transparent={true}>
+          <View style={styles.modalContainer}>
+            <Image
+              source={require("../assets/SmartWallet.png")}
+              style={{
+                width: 250,
+                height: 150,
+                marginBottom: 30,
+                alignSelf: "center",
+              }}
+              resizeMode="contain"
+            />
+            <Text style={styles.modalText}>
+              Tem certeza que deseja excluir?
+            </Text>
+            <View style={styles.modalButtonContainer}>
+              <Button title="Cancelar" onPress={closeModal} />
+              <Button title="Confirmar" onPress={handleConfirmDelete} />
+            </View>
+          </View>
+        </Modal>
       </ScrollView>
-      <View style={{marginTop: 70,}}>
-      <FooterNavigation navigation={navigation} />
+      <View style={{ marginTop: 70 }}>
+        <FooterNavigation navigation={navigation} />
       </View>
     </View>
   );
@@ -204,11 +226,12 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     margin: 5,
     marginTop: 15,
-    marginLeft: 20,
-    marginRight: 20,
+    marginLeft: 25,
+    marginRight: 25,
     paddingTop: 20,
     paddingBottom: 20,
-    paddingLeft: 15,
+    paddingLeft: 25,
+    paddingRight: 25,
   },
   cardContainer: {
     flexDirection: "row",
@@ -230,7 +253,7 @@ const styles = StyleSheet.create({
   },
   editButton: {
     backgroundColor: "white",
-    marginLeft: 290,
+    marginLeft: 250,
     marginRight: 10,
     borderRadius: 10,
   },
@@ -256,19 +279,19 @@ const styles = StyleSheet.create({
   },
   modalContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'darkblue',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "darkblue",
   },
   modalText: {
     fontSize: 18,
     marginBottom: 20,
-    color: 'white',
+    color: "white",
   },
   modalButtonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '80%',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "80%",
   },
 });
 
--- src/pages/Usuario.js
+++ src/pages/Usuario.js
@@ -9,7 +9,6 @@ import { getLancamentos } from "../services/lancamento.services";
 import FooterNavigation from "../components/footer";
 import Header from "../components/Header";
 import { IconButton } from "react-native-paper";
-import Icon from "react-native-vector-icons/MaterialIcons";
 import Swiper from 'react-native-swiper';
 
 const Usuario = () => {
@@ -37,6 +36,11 @@ const Usuario = () => {
       const somaSaldo = saldoParaReceita - saldoParaDespesa;
       const saldo = setCurrencyFormat(somaSaldo);
       setSaldoTotal(saldo);
+
+      const percentualDespesas = (saldoParaDespesa / saldoParaReceita) * 100;
+      if (percentualDespesas > 75) {
+        alert("As despesas ultrapassaram 75% das suas receitas!");
+      }
     });
   }, [isFocused, userId]);
 
@@ -98,6 +102,7 @@ const Usuario = () => {
         subtitleStyle={{
           color: "white",
           fontSize: 30,
+          fontWeight: 900,
           paddingTop: 10,
           paddingBottom: 20,
         }}
@@ -141,9 +146,20 @@ const Usuario = () => {
         </Card>
       </View>
 
+      <Divider
+        style={{
+          borderColor: "gray",
+          borderWidth: 0.2,
+          marginLeft: 20,
+          marginRight: 20,
+          marginBottom: 0,
+          marginTop: 10,
+        }}
+      />
+
       <Text style={styles.evolucao}>Evolução Mensal</Text>
 <View>
-<VictoryChart width={430} height={230} theme={VictoryTheme.material}>
+<VictoryChart width={430} height={200} theme={VictoryTheme.material}>
           <VictoryBar
             data={data}
             x="x"
@@ -156,8 +172,8 @@ const Usuario = () => {
 </View>
 <Divider
         style={{
-          borderColor: "darkblue",
-          borderWidth: 0.5,
+          borderColor: "gray",
+          borderWidth: 0.2,
           marginLeft: 20,
           marginRight: 20,
           marginBottom: 0,
@@ -166,37 +182,24 @@ const Usuario = () => {
       />
 
       <Text style={styles.evolucao}>Próximos Eventos</Text>
-
-<Swiper style={styles.wrapper} showsButtons={true}>
-      <View style={styles.slide1}>
-        <Text style={styles.text}>Energia Elétrica
-        </Text>
+      <View style={{paddingBottom: 50, marginBottom: 50,}}>
+      <Swiper showsButtons={true}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Energia Elétrica</Text>
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Slide 2</Text>
+        </View>
+      </Swiper>
       </View>
-      <View style={styles.slide2}>
-        <Text style={styles.text}>Slide 2</Text>
-      </View>
-    </Swiper>
 
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "flex-end",
-          alignItems: "center",
-          margin: 10,
-        }}
+      <TouchableOpacity
+        style={styles.chatButton}
+        onPress={() => navigation.navigate("Chat")}
       >
-        <TouchableOpacity
-          style={styles.chatButton}
-          onPress={() => navigation.navigate("Chat")}
-        >
-          <IconButton icon="chat-processing" size={35} color="#000080" />
-          <Text
-            style={{ textAlign: "center", color: "darkblue", marginLeft: 5, paddingBottom: 10,}}
-          >
-            CHAT
-          </Text>
-        </TouchableOpacity>
-      </View>
+        <IconButton icon="chat-processing" size={50} color="#000080" />
+      </TouchableOpacity>
+
       <FooterNavigation navigation={navigation} />
     </View>
   );
@@ -208,66 +211,87 @@ const styles = StyleSheet.create({
     justifyContent: "space-evenly",
   },
   card: {
-    backgroundColor: "#000080",
-    color: "white",
-    alignContent: "center",
+    backgroundColor: "darkblue",
+    borderColor: "whitesmoke",
+    borderWidth: 0.3,
     borderRadius: 17,
+    alignContent: "center",
     margin: 5,
-    marginTop: 15,
-    marginLeft: 10,
-    marginRight: 10,
+    marginLeft: 35,
+    marginRight: 35,
   },
   cardContainer: {
     flexDirection: "row",
     margin: 5,
     justifyContent: "center",
+    marginBottom: 0,
+    paddingBottom: 0,
   },
   receita: {
     backgroundColor: "#F0FFF0",
-    width: "50%",
+    width: "43%",
+    borderWidth: 0.3,
     borderRadius: 17,
-    marginLeft: 20,
+    marginLeft: 40,
+    marginRight: 7,
+    textAlign: "left",
+    alignContent: "space-between",
   },
   despesa: {
     backgroundColor: "#FFC0CB",
-    width: "50%",
+    width: "43%",
+    borderWidth: 0.3,
     borderRadius: 17,
-    marginRight: 30,
+    marginRight: 40,
   },
   verExtratoText: {
     color: "white",
     fontSize: 12,
     marginRight: 10,
+    fontWeight: 900,
   },
   evolucao: {
     fontSize: 15,
-    padding: 15,
+    margin: 15,
     fontWeight: "700",
     color: "darkblue",
     textAlign: "left",
+    marginTop: 10,
+    marginBottom: 10,
   },
   wrapper: {
-  height: 300,
   },
   slide1: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'aliceblue',
-    height: 300,
+    backgroundColor: '#9DD6EB',
   },
   slide2: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'aliceblue',
+    backgroundColor: '#97CAE5',
   },
   text: {
-    color: 'darkblue',
-    fontSize: 15,
+    color: '#fff',
+    fontSize: 30,
     fontWeight: 'bold',
-    textAlign: "left",
+  },
+  chatButton: {
+    position: 'absolute',
+    bottom: 50,
+    right: 10,
+    zIndex: 2,
   },
 });
 
 export default Usuario;
+
+
+/*style={{
+  flexDirection: "row",
+  justifyContent: "flex-end",
+  alignItems: "center",
+  margin: 10,
+}}*/
